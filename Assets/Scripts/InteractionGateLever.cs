84dd843add73d25cd15a983b9035a0692323059a
Emanuele Cecchia <emanuelececchia@gmail.com>
2024-01-23 00:18:49 +0100

    just added lever for the gate and added E for the interaction in the input system

---
+++ Assets/Scripts/InteractionGateLever.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM 
+using UnityEngine.InputSystem;
+#endif
+
+public class SwitchGateLever : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
