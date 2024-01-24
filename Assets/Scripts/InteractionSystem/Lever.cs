d23b9e5e635b2aa6bc5c10a0a9af7284cb04892c
Emanuele Cecchia <emanuelececchia@gmail.com>
2024-01-24 22:32:32 +0100

    animation lever when interacted with

--- Assets/Scripts/InteractionSystem/Lever.cs
+++ Assets/Scripts/InteractionSystem/Lever.cs
@@ -7,9 +7,16 @@ public class Lever : MonoBehaviour, IInteractable
     [SerializeField] private string _prompt;
     public string InteractionPrompt => _prompt;
 
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     public bool Interact(Interactor interactor)
     {
-        Debug.Log("Interacted with lever");
+        _animator.SetTrigger("Pull");
         return true;
     }
 }
