842dc5b42fbd87fe82e81d839f66446c58c3179d
Emanuele Cecchia <emanuelececchia@gmail.com>
2024-01-30 16:41:08 +0100

    added sound to opening gates

--- Assets/Scripts/InteractionSystem/Lever.cs
+++ Assets/Scripts/InteractionSystem/Lever.cs
@@ -10,18 +10,23 @@ public class Lever : MonoBehaviour, IInteractable
     [SerializeField] private bool _canInteract;
     public bool canInteract => _canInteract;
 
-    public Animator _gateAnimator;
+    public GameObject _gate;
+    private Animator _gateAnimator;
+    private AudioSource _gateSound;
     private Animator _animator;
 
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _gateAnimator = _gate.GetComponent<Animator>();
+        _gateSound = _gate.GetComponent<AudioSource>();
     }
 
     public bool Interact(Interactor interactor)
     {
         _animator.SetTrigger("Pull");
         if (_gateAnimator != null) _gateAnimator.SetTrigger("Open");
+        if (_gateSound != null) _gateSound.Play();
         _canInteract = false;
         return true;
     }
---
+++ Assets/Scripts/StopAudioWhenAnimationFinish.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopAudioWhenAnimationFinish : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // When normalizedTime is greater than or equal to 1.0, it means that the animation has completed one full cycle.
+        // The !animator.IsInTransition(0) check ensures that the animation is not in a transition phase between states.
+        if (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !_animator.IsInTransition(0))
+        {
+            _audioSource.Stop();
+        }
+    }
+}
