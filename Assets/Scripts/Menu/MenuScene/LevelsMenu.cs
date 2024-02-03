d1438226aaee77aac8a36ba3dd65d6d8d2944ec8
Emanuele Cecchia <emanuelececchia@gmail.com>
2024-02-03 21:56:21 +0100

    added level record in the menu; also minor fixes

--- Assets/Scripts/Menu/MenuScene/LevelsMenu.cs
+++ Assets/Scripts/Menu/MenuScene/LevelsMenu.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelsMenu : MonoBehaviour
 {
+    public TextMeshProUGUI castleRecordText;
+
     public void PlayTutorial()
     {
         LoadScene("Tutorial");
@@ -28,5 +31,17 @@ public class LevelsMenu : MonoBehaviour
     private void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName); //  LoadSceneMode.Single (default) or LoadSceneMode.Additive
+    }
+
+    private void Start()
+    {
+        castleRecordText.text = TimeFromFloatToString(PlayerPrefs.GetFloat("BestTimeCastle"));
+    }
+
+    string TimeFromFloatToString(float time)
+    {
+        string minutes = Mathf.Floor(time / 60).ToString("00");
+        string seconds = (time % 60).ToString("00");
+        return minutes + ":" + seconds;
     }
 }
