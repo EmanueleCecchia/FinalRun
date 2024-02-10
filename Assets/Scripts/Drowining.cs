9d1db583e5bfe4d037553fb3c5cd7088bece1bbf
Emanuele Cecchia <emanuelececchia@gmail.com>
2024-02-10 16:30:28 +0100

    refactoring drowinig script

--- Assets/Scripts/Drowining.cs
+++ Assets/Scripts/Drowining.cs
@@ -6,20 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class Drowining : MonoBehaviour
 {
-
-    private GameObject player;
-    private Transform playerTransform;
-    private Quaternion originalEnemyRotation;
     [SerializeField] private float respawnTime = 2f;
-    [SerializeField] private Vector3 respawnPosition;
-
     [SerializeField] private GameObject respawnCanvas;
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        playerTransform = player.transform;
-
         respawnCanvas.SetActive(false);
     }
     private void OnTriggerEnter(Collider other)
